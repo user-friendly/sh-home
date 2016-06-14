3bcb28c1ca6ee69d029f3770e2911510060e09b2
Plamen Ivanov <plamen.d.ivanov@gmail.com>
2016-06-14 11:33:14 -0400

    Emacs, add Magit package configurations.

--- .emacs.d/custom/setup-programming.el
+++ .emacs.d/custom/setup-programming.el
@@ -28,3 +28,29 @@
 ;; Web Mode
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGE: magit                       ;;
+;;                                      ;;
+;; GROUP: Programming -> Tools -> Magit ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'magit)
+(set-default 'magit-stage-all-confirm nil)
+(add-hook 'magit-mode-hook 'magit-load-config-extensions)
+
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(global-unset-key (kbd "C-x g"))
+(global-set-key (kbd "C-x g h") 'magit-log)
+(global-set-key (kbd "C-x g f") 'magit-file-log)
+(global-set-key (kbd "C-x g b") 'magit-blame-mode)
+(global-set-key (kbd "C-x g m") 'magit-branch-manager)
+(global-set-key (kbd "C-x g c") 'magit-branch)
+(global-set-key (kbd "C-x g s") 'magit-status)
+(global-set-key (kbd "C-x g r") 'magit-reflog)
+(global-set-key (kbd "C-x g t") 'magit-tag)
+
